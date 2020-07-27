555a2814beac3ff82a97ff42c9f9f47360ffe008
Sudhir <asudhir@ford.com>
2020-07-27 09:26:36 +0530

    Update TestNg.java
    Added testng annotations to the class file

--- test/java/com/ford/auto/pageobjects/TestNg.java
+++ test/java/com/ford/auto/pageobjects/TestNg.java
@@ -1,7 +1,7 @@
 package com.ford.auto.pageobjects;
-
+import org.testng.annotations.Test;
 public class TestNg {
-
+    @Test
 	public void test() {
 		
 		System.out.println("Hello this is a testng class");
